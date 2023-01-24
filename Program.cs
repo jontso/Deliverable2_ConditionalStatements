78246b5bb165db05c00ee0c4f0d98fc2740aa5a6
jontso <122553409+jontso@users.noreply.github.com>
2023-01-23 23:37:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Author: Jonathan Tsokos
+Date: 1/22/23
+Description: C# Console Application for calculating letter grades
+*/
+
+using System;
+
+namespace deliverable2_conditionalstatements;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a numeric grade: ");
+
+        try
+        {
+            decimal grade = decimal.Parse(Console.ReadLine());
+
+            string letter;
+
+            if (grade >= 90)
+            {
+                letter = "A";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+            }
+
+            else if (grade >= 80 && grade < 90)
+            {
+                letter = "B";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+            }
+
+            if (grade >= 70 && grade < 80)
+            {
+                letter = "C";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+            }
+            else if (grade >= 60 && grade < 70)
+            {
+                letter = "D";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+            }
+            if (grade >= 0 && grade < 60)
+            {
+                letter = "F";
+                Console.WriteLine("Your expected letter grade for ISM 4300 is: " + letter);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Enter a numeric value");
+        }
+    }
+}
